86e03ff5f9c9fd1f590e6374a0f1afcccebeeb30
neosome <167439712+neosomee@users.noreply.github.com>
2025-04-10 17:49:50 +0300

    Update test_classes.py

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.classes import Product, Category
 from unittest.mock import patch
 
@@ -22,6 +24,8 @@ def test_count_category():
     product4 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
     category1.add_product(product4)
     assert category1.product_count == 4
+    with pytest.raises(TypeError):
+        category1.add_product({})
 
 
 def test_init_product(product):
@@ -47,59 +51,16 @@ def test_product_add_new():
     assert new_product.price == 180000.0
 
 
-
-import pytest
-from src.classes import Category, Product
-
-@pytest.fixture
-def product1():
-    return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", "180000.0", 5)
-
-@pytest.fixture
-def product2():
-    return Product("Iphone 15", "512GB, Gray space", "210000.0", 8)
-
-@pytest.fixture
-def product3():
-    return Product("Xiaomi Redmi Note 11", "1024GB, Синий", "31000.0", 14)
-
-@pytest.fixture
-def product4():
-    return Product("55\" QLED 4K", "Фоновая подсветка", "123000.0", 7)
-
-def test_product_init(product1):
-    assert product1.name == "Samsung Galaxy S23 Ultra"
-    assert product1.description == "256GB, Серый цвет, 200MP камера"
-    assert product1.price == "180000.0"
-    assert product1.quantity == 5
-
-def test_product_init2(product2):
-    assert product2.name == "Iphone 15"
-    assert product2.description == "512GB, Gray space"
-    assert product2.price == "210000.0"
-    assert product2.quantity == 8
-
-def test_category_init(product1, product2, product3):
-    category = Category("Смартфоны", "Описание категории", [product1, product2, product3])
-    assert category.name == "Смартфоны"
-    assert category.description == "Описание категории"
-    assert len(category.products) == 3
-
-def test_category_product_count(product1, product2, product3):
-    category = Category("Смартфоны", "Описание категории", [product1, product2, product3])
-    assert category.product_count() == 3
-
-def test_category_count(product1, product2, product3, product4):
-    Category.category_count = 0
-    Category.total_products = 0
-    category1 = Category("Смартфоны", "Описание категории", [product1, product2, product3])
-    category2 = Category("Телевизоры", "Описание категории", [product4])
-    assert Category.category_count == 2
-    assert Category.total_products == 4
-
-def test_total_products(product1, product2, product3, product4):
-    Category.category_count = 0
-    Category.total_products = 0
-    category1 = Category("Смартфоны", "Описание категории", [product1, product2, product3])
-    category2 = Category("Телевизоры", "Описание категории", [product4])
-    assert Category.total_products == 4
+@patch('builtins.input')
+def test_product_price_set(mock_input,capsys):
+    product = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    assert product.price == 210000.0
+    product.price = -100
+    captured = capsys.readouterr()
+    assert captured.out == 'Цена не должна быть нулевая или отрицательная\n'
+    mock_input.return_value = 'y'
+    product.price = 1000
+    assert product.price == 1000
+    mock_input.return_value = 'n'
+    product.price = 800
+    assert product.price == 1000
